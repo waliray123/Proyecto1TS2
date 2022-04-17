77596add487737c00d2698beb4a53e2447704939
waliray123 <willy2000.wld@gmail.com>
2022-04-17 10:51:19 -0600

    Login agregado

--- MayaWeb/index.js
+++ MayaWeb/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({extended:false}));
-app.use(express.json);
-
 
 const dotenv = require('dotenv');
 dotenv.config({path:'./env/.env'});
@@ -17,17 +15,114 @@ const bcryptjs = require('bcryptjs');
 
 const session = require('express-session');
 app.use(session({
-  secret:'laclavesecretaespepe',
-  resave: 'true',
-  saveUninitialized: 'true'
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
 }));
 
 const connection = require('./utils/db');
 
-app.get('/',(req,res)=>{
-  res.render('HOLA');
-})
 
-app.listen(3000,(req,res)=>{
-  console.log('SERVER RUNNING IN http://localhost:3000');
-})
+app.get('/login',(req, res)=>{
+	res.render('login');
+});
+
+app.get('/register',(req, res)=>{
+	res.render('register');
+});
+
+app.post('/register', async (req, res)=>{
+	const user = req.body.user;
+	const name = req.body.name;
+    const rol = req.body.rol;
+	const pass = req.body.pass;
+	let passwordHash = await bcryptjs.hash(pass, 8);
+    connection.query('INSERT INTO Usuario SET ?',{nombre_usuario:user, nombre:name, apellido:rol, password:passwordHash}, async (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{            
+			res.render('register', {
+				alert: true,
+				alertTitle: "Registration",
+				alertMessage: "¡Successful Registration!",
+				alertIcon:'success',
+				showConfirmButton: false,
+				timer: 1500,
+				ruta: ''
+			});
+            //res.redirect('/');         
+        }
+	});
+});
+
+app.post('/auth', async (req, res)=> {
+	const user = req.body.user;
+	const pass = req.body.pass;    
+    let passwordHash = await bcryptjs.hash(pass, 8);
+	if (user && pass) {
+		connection.query('SELECT * FROM Usuario WHERE nombre_usuario = ?', [user], async (error, results, fields)=> {
+			if( results.length == 0 || !(await bcryptjs.compare(pass, results[0].password)) ) {    
+				res.render('login', {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "USUARIO y/o PASSWORD incorrectas",
+                        alertIcon:'error',
+                        showConfirmButton: true,
+                        timer: false,
+                        ruta: 'login'    
+                    });
+				
+				//Mensaje simple y poco vistoso
+                //res.send('Incorrect Username and/or Password!');				
+			} else {         
+				//creamos una var de session y le asignamos true si INICIO SESSION       
+				req.session.loggedin = true;                
+				req.session.nombre_usuario = results[0].name;
+				res.render('login', {
+					alert: true,
+					alertTitle: "Conexión exitosa",
+					alertMessage: "¡LOGIN CORRECTO!",
+					alertIcon:'success',
+					showConfirmButton: false,
+					timer: 1500,
+					ruta: ''
+				});        			
+			}			
+			res.end();
+		});
+	} else {	
+		res.send('Please enter user and Password!');
+		res.end();
+	}
+});
+
+app.get('/', (req, res)=> {
+	if (req.session.loggedin) {
+		res.render('index',{
+			login: true,
+			name: req.session.name			
+		});		
+	} else {
+		res.render('index',{
+			login:false,
+			name:'Debe iniciar sesión',			
+		});				
+	}
+	res.end();
+});
+
+app.use(function(req, res, next) {
+    if (!req.user)
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
+
+app.get('/logout', function (req, res) {
+	req.session.destroy(() => {
+	  res.redirect('/') // siempre se ejecutará después de que se destruya la sesión
+	})
+});
+
+app.listen(3000, (req, res)=>{
+    console.log('SERVER RUNNING IN http://localhost:3000');
+});
--- MayaWeb/utils/db.js
+++ MayaWeb/utils/db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+const connection = mysql.createConnection({    
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
 });
 
 connection.connect((error)=>{
-    if(error){
-        console.log('Error en la conexion base de datos: ' + error);
-        return;
+    if (error) {
+      console.error('El error de conexión es: ' + error);
+      return;
     }
-    console.log('Conectado de la base de datos');
-});
+    console.log('¡Conectado a la Base de Datos!');
+  });
 
-module.exports = connection;
+  module.exports = connection;
